822991b1070b0df87ffe9b6d67da08d9a14a752b
tesiornajonmar-oss <tesiornajonmar@gmail.com>
2026-07-02 20:16:43 +0800

    edit

--- IngredientRepo.java
+++ IngredientRepo.java
@@ -6,9 +6,9 @@ import java.util.List;
 public interface IngredientRepo {
     Ingredient findById(int id);
     List<Ingredient> findAll();
-    void save(Ingredient ingredient);
-    void update(Ingredient ingredient);
-    void delete(int id);
+    public abstract void save(Ingredient ingredient);
+    public abstract void update(Ingredient ingredient);
+    public abstract void delete(int id);
 
     List<Ingredient> findBySupplierId(int supplierId);
     List<Ingredient> findLowStockIngredients();
